ecd5c5f0782e62a9e4fd29175cbef10acab30dfc
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-12 23:12:33 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
