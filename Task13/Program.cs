3b8598e3b31d8bc2fc2f12519cd2662c41cc5f3f
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-12 22:56:42 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int length = number.Length;
+if (length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(number[2]);
+}
+
+
